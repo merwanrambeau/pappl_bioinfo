94407e328f98ca374a1b996ed5d8938dd3e57847
MerwanRambeau <merwan.rambeau@gmail.com>
2014-01-23 12:05:46 +0100

    added type and name to main request

--- src/MainTest.java
+++ src/MainTest.java
@@ -27,10 +27,14 @@ public class MainTest {
 			Connection con=DriverManager.getConnection(dbURL,"root","papplsql");
 			
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT reformatted_entity_particpant.participantId, reformatted_entity_particpant.entityId, reformatted_abstract_node.superNodeId, reformatted_entity_particpant.pathwaydbId FROM reformatted_entity_particpant JOIN reformatted_abstract_node ON reformatted_abstract_node.participantId=reformatted_entity_particpant.participantId WHERE reformatted_abstract_node.pathwayDbId='2' AND reformatted_entity_particpant.pathwaydbId='2';");
+			ResultSet rs = stmt.executeQuery("SELECT reformatted_entity_particpant.participantId, reformatted_entity_particpant.entityId, reformatted_abstract_node.superNodeId, reformatted_entity_particpant.pathwaydbId, reformatted_entity_information.entityType, reformatted_entity_information.entityName "
+					+ "FROM reformatted_entity_particpant "
+					+ "JOIN reformatted_abstract_node ON reformatted_abstract_node.participantId=reformatted_entity_particpant.participantId "
+					+ "JOIN reformatted_entity_information ON reformatted_entity_information.entityId = reformatted_entity_particpant.entityId"
+					+ " WHERE reformatted_abstract_node.pathwayDbId='2' AND reformatted_entity_particpant.pathwaydbId='2';");
 			SimpleDirectedGraph<Node,Edge> graph = new SimpleDirectedGraph<Node,Edge>(Edge.class);
 			while (rs.next()) {
-				Node n = new Node(rs.getString(1), rs.getString(2),rs.getString(3),rs.getInt(4));
+				Node n = new Node(rs.getString(1), rs.getString(2),rs.getString(3),rs.getInt(4), rs.getString(5), rs.getString(6));
 				graph.addVertex(n);
 			}
 			rs = stmt.executeQuery("SELECT * FROM reformatted_pathway_relationPair WHERE pathwayDbId='2';");
